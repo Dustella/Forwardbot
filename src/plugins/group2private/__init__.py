fe0a3df65c92d86069c390c5c219b52cb70e8cdc
Dustella <fdnoaivj@outlook.com>
2021-09-30 00:37:42 +0800

    fix g2p

--- src/plugins/group2private/__init__.py
+++ src/plugins/group2private/__init__.py
@@ -14,24 +14,13 @@ reply_session=on_message()
 async def g2p(bot:Bot,event=GroupMessageEvent):
     raw=event.raw_message
     if 'CQ:reply' in raw:
-        print('OK')
-        # try:
-        print(raw)
-        msg_id_pattern = re.compile(r'(?<=id=).+(?=\])')
-        msgid=msg_id_pattern.search(raw)
-        print(msgid)
-        reply_msg_id=raw[msgid.start():msgid.end()]
-        originnal_content=await bot.get_msg(message_id=reply_msg_id)
+        msgid=event.json()["reply"]["message_id"]
+        originnal_content=await bot.get_msg(message_id=msgid)
         txt=originnal_content['message'][0]['data']['text']
-        print('')
         sender_id_pattern = re.compile(r'(?<=\$).+(?=\n)')
         id_send=txt[sender_id_pattern.search(txt).start():sender_id_pattern.search(txt).end()]
         tosend=str(event.message).replace('[CQ:at,qq=2285047281] ', '')
-        print(tosend)
         await bot.send_private_msg(user_id=id_send,message=tosend)
-        # except:
-        #     print('error')
-
 
 
 
