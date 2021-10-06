541375d52edf29fc4d06b9b6884dde0af01c3cbd
Dustella <fdnoaivj@outlook.com>
2021-10-06 20:59:36 +0800

    del_msg is ok

--- src/plugins/group2private/__init__.py
+++ src/plugins/group2private/__init__.py
@@ -6,27 +6,35 @@ from nonebot.exception import ActionFailed
 import re
 from .config import Config
 
+
 global_config = get_driver().config
 config = Config(**global_config.dict())
-
-callin_session =on_message(rule=to_me())
+callin_session =on_message(rule=to_me(),priority=1)
 
 @callin_session.handle()
 async def handle_first_message(bot:Bot,event:GroupMessageEvent,state:T_State):
     if str(event.message).startswith('#'):await callin_session.finish()
     # if reply is a comment, do nothing
-
     origin=str(event.reply.message)
-    sender_id_pattern = re.compile(r'(?<=@).+(?=\n)')
+    sender_id_pattern = re.compile(r'(?<=\@).+(?=\n)')
+    message_id_pattern = re.compile(r'(?<=#).+(?=\n)')
     state["target_id"]=origin[sender_id_pattern.search(origin).start():sender_id_pattern.search(origin).end()]
+    state["message_id"]=origin[message_id_pattern.search(origin).start():message_id_pattern.search(origin).end()]
     # get reply target_id, stored in state
-
-    if not str(event.message)=="pic":
+    print(state)
+    if str(event.message)=="pic":
+        state["is_pic"]=True
+    elif str(event.message)=="del" or str(event.message)=="recall":
+        try:
+            await bot.delete_msg(message_id=state["message_id"])
+            await callin_session.finish()
+        except ActionFailed as err:
+            await callin_session.finish(f"Error encountered, {err}")
+    else:
         state["message"] =event.message
         state["is_pic"]=False
-    else:
-        state["is_pic"]=True
-    # handle pictures
+        # handle pictures
+
 
 @callin_session.got("message",prompt="Send a picture")
 async def send_picture(bot:Bot,event:GroupMessageEvent,state:T_State):
@@ -35,11 +43,10 @@ async def send_picture(bot:Bot,event:GroupMessageEvent,state:T_State):
     and not "cancel" in str(state["message"])):
         callin_session.reject("Please send a picture")
         # if message is not a picture, reject it
-    
+
     try:
         await bot.send_private_msg(user_id=state["target_id"],message=state["message"])
         await callin_session.finish()
         # final send
     except ActionFailed as err:
         await bot.finish(f"Error encountered, {err}")
-        # if Error is encountered, log the error
