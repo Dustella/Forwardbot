1f3eb74c079c8b77891fe9e9fafc8967fcdcc0d6
Dustella <fdnoaivj@outlook.com>
2021-10-14 20:30:22 +0800

    reverted changes

--- src/plugins/private2group/__init__.py
+++ src/plugins/private2group/__init__.py
@@ -14,9 +14,9 @@ status_config = Config(**global_config.dict())
 @callin_session.handle()
 async def p2g(bot:Bot,event:PrivateMessageEvent):
     global last_callin_session
-    last_message_user=(await bot.get_group_msg_history(group_id=status_config.group_id))["messages"][19]["user_id"]
-    last_message=(await bot.get_group_msg_history(group_id=status_config.group_id))["messages"][19]["raw_message"]
-    lite_flag=(str(last_message_user)==str(bot.self_id)) and (str(last_message).find("Sent_to")==-1) and (last_callin_session==event.get_user_id())
+    # last_message_user=(await bot.get_group_msg_history(group_id=status_config.group_id))["messages"][19]["user_id"]
+    # last_message=(await bot.get_group_msg_history(group_id=status_config.group_id))["messages"][19]["raw_message"]
+    # lite_flag=(str(last_message_user)==str(bot.self_id)) and (str(last_message).find("Sent_to")==-1) and (last_callin_session==event.get_user_id())
 
     user_info=f"From: {event.sender.nickname}\n@{event.get_user_id()}#{event.message_id}\n"
     message_type=json.loads(event.json())["message"][0]["type"]
@@ -26,7 +26,7 @@ async def p2g(bot:Bot,event:PrivateMessageEvent):
         await bot.send_group_msg(group_id=status_config.group_id,message=f"{user_info}\n[转发消息]")
     elif message_type=="xml":
         await bot.send_group_msg(group_id=status_config.group_id,message=f"{user_info}\n[xml消息]")
-    fwd_str=(f'{user_info}\n{event.get_message()}',f"{event.get_message()}")[lite_flag]
+    fwd_str=(f'{user_info}\n{event.get_message()}',f"{event.get_message()}")[0]
 
     last_callin_session=event.get_user_id()
     await bot.send_group_msg(group_id=status_config.group_id,message=fwd_str)
