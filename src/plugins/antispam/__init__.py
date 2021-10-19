139a9ca3af486620dc4ebbb7e938b9db862576c2
Dustella <fdnoaivj@outlook.com>
2021-10-19 14:46:16 +0800

    added spam userid

--- src/plugins/antispam/__init__.py
+++ src/plugins/antispam/__init__.py
@@ -13,11 +13,13 @@ spam = on_keyword({"还没进群的同学", "学校有重要事宜通知"})
 @spam.handle()
 async def anti_spam(bot: Bot, event: MessageEvent):
     this_message = event.message
+    user_id=event.sender.user_id
+    user_nick=event.sender.nickname
     if str(event.sender.user_id) == str(bot.self_id):
         spam.finish()
     try:
         await bot.delete_msg(message_id=event.message_id)
-        await spam.finish(f"可能的诈骗信息已识别，请勿上当。\n原消息已撤回,内容：\n{str(this_message)}")
+        await spam.finish(f"可能的诈骗信息已识别，请勿上当。\n消息发送者：{user_nick}@{user_id}\n原消息已撤回,内容：\n{str(this_message)}")
     except ActionFailed as err:
-        await spam.finish(f"可能的诈骗信息已识别，请勿上当。\n原消息无法撤回,内容：\n{str(this_message)}\n无法撤回原因：{str(err)}")
+        await spam.finish(f"可能的诈骗信息已识别，请勿上当。\n消息发送者：{user_nick}@{user_id}\n原消息无法撤回,内容：\n{str(this_message)}\n无法撤回原因：{str(err)}")
     pass
