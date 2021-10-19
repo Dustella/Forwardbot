749829d248f87d86bbd9a08880605fcfaa240536
Dustella <fdnoaivj@outlook.com>
2021-10-19 14:33:22 +0800

    prevent flood messaging

--- src/plugins/antispam/__init__.py
+++ src/plugins/antispam/__init__.py
@@ -1,17 +1,20 @@
 # import nonebot
-from nonebot import get_driver,on_keyword
+from nonebot import get_driver, on_keyword
 from nonebot.exception import ActionFailed
-from nonebot.adapters.cqhttp import Bot,MessageEvent
+from nonebot.adapters.cqhttp import Bot, MessageEvent
 from .config import Config
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
 
-spam=on_keyword({"还没进群的同学","学校有重要事宜通知"})
+spam = on_keyword({"还没进群的同学", "学校有重要事宜通知"})
+
 
 @spam.handle()
-async def anti_spam(bot:Bot,event:MessageEvent):
+async def anti_spam(bot: Bot, event: MessageEvent):
     this_message = event.message
+    if str(event.sender.user_id) == str(bot.self_id):
+        spam.finish()
     try:
         await bot.delete_msg(message_id=event.message_id)
         await spam.finish(f"可能的诈骗信息已识别，请勿上当。\n原消息已撤回,内容：\n{str(this_message)}")
